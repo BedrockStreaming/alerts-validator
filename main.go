e0b0119a8fed868f020a83178093f291b86f91cb
Tanguy Falconnet <tfalconnet.externe@bedrockstreaming.com>
2022-07-22 14:36:14 +0200

    refacto: almost the same but better

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 	"strings"
@@ -18,6 +17,9 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"gopkg.in/yaml.v2"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 type HTTPClient interface {
@@ -25,16 +27,18 @@ type HTTPClient interface {
 }
 
 type Config struct {
-	ListenAddress string   `yaml:"listenAddress"`
-	ListenPort    int      `yaml:"listenPort"`
-	Interval      int      `yaml:"interval"`
-	Servers       []Server `yaml:"servers"`
+	ListenAddress          string   `yaml:"listenAddress"`
+	ListenPort             int      `yaml:"listenPort"`
+	ComputeInterval        string   `yaml:"computeInterval"`
+	ValidityCheckIntervals []string `yaml:"validityCheckIntervals"`
+	Servers                []Server `yaml:"servers"`
+	LabelKeys              []string `yaml:"labelKeys"`
 }
 
 type Server struct {
-	Tenant    string `yaml:"tenant"`
-	AlertURL  string `yaml:"alertUrl"`
-	SelectURL string `yaml:"selectUrl"`
+	LabelValues []string `yaml:"labelValues"`
+	AlertURL    string   `yaml:"alertUrl"`
+	SelectURL   string   `yaml:"selectUrl"`
 }
 
 type Groups struct {
@@ -61,81 +65,71 @@ type Query struct {
 var (
 	vectors []string
 	Client  HTTPClient
-
-	metric_present_1_day = promauto.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "alertsvalidator_present_over_1day",
-			Help: "does the alert's metrics contains data point over 1 day",
-		},
-		[]string{
-			"tenant",
-			"alertname",
-		},
-	)
-	metric_present_30_days = promauto.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "alertsvalidator_present_over_30days",
-			Help: "does the alert's metric contains data point over the last 30 days",
-		},
-		[]string{
-			"tenant",
-			"alertname",
-		},
-	)
-	metric_present_90_days = promauto.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "alertsvalidator_present_over_90days",
-			Help: "does the alert's metric contains data point over the last 90 days",
-		},
-		[]string{
-			"tenant",
-			"alertname",
-		},
-	)
+	gauge   *prometheus.GaugeVec
+	config  Config
 )
 
 func main() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 	confPath := flag.String("conf", "config.yaml", "Config path")
+	debug := flag.Bool("debug", false, "sets log level to debug")
 	flag.Parse()
 
-	config := loadConf(*confPath)
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	if *debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+
+	config = *loadConf(*confPath)
 	listenAddress := config.ListenAddress
 	listenPort := config.ListenPort
-	interval := config.Interval
 
 	// Init http client
 	Client = &http.Client{}
 
+	// Build Gauges
+	gauge = promauto.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "alertsvalidator_validity_range",
+			Help: "Does the alert's metric contains data point in range",
+		},
+		append([]string{
+			"alertname",
+			"range_from",
+			"range_to",
+		}, config.LabelKeys...),
+	)
+
 	for _, server := range config.Servers {
-		go checkRules(server, interval)
+		go checkRules(server)
 	}
 
-	log.Println("Listening on " + listenAddress + ":" + fmt.Sprint(listenPort) + "/metrics")
+	log.Info().Msgf("Listening on %s:%d/metrics", listenAddress, listenPort)
 	http.Handle("/metrics", promhttp.Handler())
 	prometheus.Unregister(collectors.NewGoCollector())
 	prometheus.Unregister(collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}))
 	err := http.ListenAndServe(listenAddress+":"+fmt.Sprint(listenPort), nil)
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatal().Err(err).Msg("Cant launch metric server")
 	}
 }
 
 func loadConf(confPath string) *Config {
 	yamlFile, err := ioutil.ReadFile(confPath)
 	if err != nil {
-		log.Printf("yamlFile.Get err #%v ", err)
+		log.Fatal().Err(err).Msg("Can't read config file")
 		os.Exit(1)
 	}
 	var c Config
 	err = yaml.Unmarshal(yamlFile, &c)
 	if err != nil {
-		log.Fatalf("Unmarshal: %v", err)
+		log.Fatal().Err(err).Msg("Unmarshal error")
 		os.Exit(1)
 	}
 	return &c
 }
 
-func checkRules(server Server, interval int) {
+func checkRules(server Server) {
 	for {
 		groups := getRules(server.AlertURL)
 		for _, group := range groups.Data.Groups {
@@ -150,23 +144,71 @@ func checkRules(server Server, interval int) {
 				if err != nil {
 					continue
 				}
-				for _, vector := range vectors {
-					metric_present_1_day.WithLabelValues(server.Tenant, rule.Name).Set(1)
-					metric_present_30_days.WithLabelValues(server.Tenant, rule.Name).Set(1)
-					metric_present_90_days.WithLabelValues(server.Tenant, rule.Name).Set(1)
-					if !existingMetric(vector, server.SelectURL) {
-						if !existingMetric("present_over_time("+vector+"[30d])", server.SelectURL) {
-							if !existingMetric("present_over_time("+vector+"[90d])", server.SelectURL) {
-								metric_present_90_days.WithLabelValues(server.Tenant, rule.Name).Set(0)
-							}
-							metric_present_30_days.WithLabelValues(server.Tenant, rule.Name).Set(0)
-						}
-						metric_present_1_day.WithLabelValues(server.Tenant, rule.Name).Set(0)
+				zDictVectorPresent := zerolog.Dict()
+				zDictValid := zerolog.Dict()
+				for key, interval := range config.ValidityCheckIntervals {
+					zDictVector := zerolog.Dict()
+					if key >= len(config.ValidityCheckIntervals)-1 {
+						continue
 					}
+					checkedVector := []string{}
+					valid := true
+					for _, vector := range vectors {
+						checked := false
+						for _, v := range checkedVector {
+							if v == vector {
+								checked = true
+							}
+						}
+						if checked {
+							continue
+						} else {
+							checkedVector = append(checkedVector, vector)
+						}
+						dur1, err := time.ParseDuration(config.ValidityCheckIntervals[key+1])
+						if err != nil {
+							log.Error().Err(err).Msg("Something is wrong with Validity Check Intervals")
+							os.Exit(1)
+						}
+						dur2, err := time.ParseDuration(interval)
+						if err != nil {
+							log.Error().Err(err).Msg("Something is wrong with Validity Check Intervals")
+							os.Exit(1)
+						}
+						dur := time.Until(time.Now().Add(dur1).Add(-dur2))
+						check := fmt.Sprintf("present_over_time(%s[%s]) offset %s", vector, dur.String(), interval)
+						_, err = metricsql.Parse(check)
+						if err != nil {
+							log.Error().Err(err).Str("check", check).Msg("Something is wrong with Validity Check Intervals")
+							os.Exit(1)
+						}
+						log.Debug().Str("alertname", rule.Name).Str("check", check).Send()
+						if !existingMetric(check, server.SelectURL) {
+							zDictVector.Bool(vector, false)
+							valid = false
+						} else {
+							zDictVector.Bool(vector, true)
+						}
+					}
+					labels := append([]string{rule.Name, interval, config.ValidityCheckIntervals[key+1]}, server.LabelValues...)
+					if !valid {
+						gauge.WithLabelValues(labels...).Set(0)
+						zDictValid.Bool(fmt.Sprintf("%s-%s", interval, config.ValidityCheckIntervals[key+1]), false)
+					} else {
+						gauge.WithLabelValues(labels...).Set(1)
+						zDictValid.Bool(fmt.Sprintf("%s-%s", interval, config.ValidityCheckIntervals[key+1]), true)
+					}
+					zDictVectorPresent.Dict(fmt.Sprintf("%s-%s", interval, config.ValidityCheckIntervals[key+1]), zDictVector)
 				}
+				log.Info().Str("alertname", rule.Name).Dict("is_vector_present", zDictVectorPresent).Dict("is_valid", zDictValid).Send()
 			}
 		}
-		time.Sleep(time.Duration(interval) * time.Minute)
+		dur, err := time.ParseDuration(config.ComputeInterval)
+		if err != nil {
+			log.Fatal().Err(err).Msg("Can't parse Compute Interval")
+			os.Exit(0)
+		}
+		time.Sleep(dur)
 	}
 }
 
@@ -178,18 +220,23 @@ func getRules(server string) Groups {
 	var gr Groups
 
 	if err != nil {
-		fmt.Println(err)
+		log.Error().Err(err)
 		return gr
 	}
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
 		b, err := io.ReadAll(res.Body)
 		if err != nil {
-			log.Fatalln(err)
+			log.Fatal().Err(err).Msg("Can't get rules, and api response body can't be read")
+		} else {
+			log.Error().Str("body", string(b)).Msg("Can't get rules")
 		}
-		fmt.Println(string(b))
 	} else {
-		json.NewDecoder(res.Body).Decode(&gr)
+		err := json.NewDecoder(res.Body).Decode(&gr)
+
+		if err != nil {
+			log.Error().Err(err).Msg("Can't get rules")
+		}
 	}
 
 	return gr
@@ -206,10 +253,16 @@ func existingMetric(query, server string) bool {
 	var qu Query
 
 	if err != nil {
+		log.Error().Err(err).Msg("Can't query VM api")
 		return false
 	}
 	defer res.Body.Close()
-	json.NewDecoder(res.Body).Decode(&qu)
+	err = json.NewDecoder(res.Body).Decode(&qu)
+
+	if err != nil {
+		log.Error().Err(err).Msg("Can't decode VM api response")
+		return false
+	}
 
 	return len(qu.Data.Result) > 0
 }
